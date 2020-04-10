6fd0212bd8cf26bd41c316c4ba5139549e75fb3b
Solhe <solenne-herve@hotmail.fr>
2020-04-10 11:01:03 +0200

    initialisation des packages et classes

---
+++ src/solhe/controller/echecsController.java
@@ -0,0 +1,16 @@
+package solhe.controller;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class echecsController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ src/solhe/model/partie.java
@@ -0,0 +1,4 @@
+package solhe.model;
+
+public class partie {
+}
